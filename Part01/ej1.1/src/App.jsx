ed4a05eb941d2d61182e6d89b7fb4c0f2b765421
emmp <emcommunitymanager@gmail.com>
2025-10-06 13:25:04 +0200

    feat: resuelto ej1.10. La importancia de extraer componentes. Tenemos un componente Statistics que recibe de App valores y renderiza StatisticsLine que solo muestra texto y valor, este si es reutilizable y, desde App solo llamamos a un componente y le pasamos todos los valores de una

--- Part01/ej1.1/src/App.jsx
+++ Part01/ej1.1/src/App.jsx
@@ -1,25 +1,61 @@
 /**
- * 1.9: unicafe paso 4
- * Cambia tu aplicación para mostrar estadísticas solo una vez que se hayan recopilado los comentarios.
+ * 1.10: unicafe paso 5
+ * Continuemos refactorizando la aplicación. Extrae los siguiente dos componentes:
+ * Button para definir los botones utilizados para enviar comentarios
+ * StatisticLine para mostrar una única estadística, por ejemplo, la puntuación media.
+ * 
+ * Para ser claros: el componente StatisticLine siempre muestra una única estadística, lo que significa que la aplicación utiliza varios componentes para representar todas las estadísticas:
  */
 
 import { use, useState } from "react"
 
-/* Componente Boton, funcion onClick + texto mostrado dentro del boton */
-const Boton = ({ handleClick, name }) => {
+/* Componente Boton, funcion onClick + texto mostrado dentro del boton. Independiente de la app */
+const Boton = ({ handleClick, text }) => {
+  return (<button onClick={handleClick}>{text}</button>)
+}
+/* Componente BotonContador, recibe funciones dependiendo los comentarios y pinta botones */
+const BotonContador = ({contadorGood, contadorNeutral, contadorBad}) => {
   return (
-    <button onClick={handleClick}>{name}</button>
+    <div>
+      <Boton handleClick={contadorGood} text="good" />
+      <Boton handleClick={contadorNeutral} text="neutral" />
+      <Boton handleClick={contadorBad} text="bad" />
+    </div>
   )
 }
 
-/* Componente estadisticas, muestra un texto y un valor */
-const Statistics = ({ estadistica, valor }) => {
-  return (<p>{estadistica}: {valor}</p>)
+/* Muestra un texto y un valor. Si el texto es 'positive' muestra un caracter de % */
+const StatisticLine = ({ text, valor }) => {
+  if (text === 'positive') {
+    return (<p>{text}: {valor} %</p>)
+  } else {
+    return (<p>{text}: {valor}</p>)
+  }
 }
 
-/* Componente de uso explusivo cuando no hay estadisticas */
-const SinStatistics = ({ estadistica, valor }) => {
-  return (<p>No feedback given</p>)
+/* Componente estadisticas, gestiona los valores llama a staticline */
+/* De esta forma, App solo llama una vez a estatistics y le pasa todos los valores */
+const Statistics = ({ valorGood, valorNeutral, valorBad }) => {
+  const all = valorGood + valorNeutral + valorBad
+  const average = (valorGood - valorBad) / all
+  const positive = ((valorGood * 100) / all)
+
+  /* Si hay comentarios que se muestren */
+  if (all != 0) {
+    return (
+      <div>
+        <StatisticLine text='good' valor={valorGood} />
+        <StatisticLine text='neutral' valor={valorNeutral} />
+        <StatisticLine text='bad' valor={valorBad} />
+        <StatisticLine text='all' valor={all} />
+        <StatisticLine text='average' valor={average.toFixed(2)} />
+        <StatisticLine text='positive' valor={positive.toFixed(2)} />
+      </div>
+    )
+    /* Si no hay comentarios, que indique que no hay feedback */
+  } else {
+    return (<p>No feedback given</p>)
+  }
 }
 
 /* Componente main - logica de negocio */
@@ -27,59 +63,21 @@ const App = () => {
   const [contadorComentarioGood, setContadorGood] = useState(0)
   const [contadorComentarioNeutral, setContadorNeutral] = useState(0)
   const [contadorComentarioBad, setContadorBad] = useState(0)
-  const [todosLosComentarios, setTodos] = useState(0)
-  const [average, setAverage] = useState(0)
 
-  const handleClickGood = () => {
-    setContadorGood(contadorComentarioGood + 1)
-    setTodos(todosLosComentarios + 1)
-    setAverage(average + 1)
-  }
-  const handleClickNeutral = () => {
-    setContadorNeutral(contadorComentarioNeutral + 1)
-    setTodos(todosLosComentarios + 1)
-    setAverage(average + 0)
-  }
-  const handleClickBad = () => {
-    setContadorBad(contadorComentarioBad + 1)
-    setTodos(todosLosComentarios + 1)
-    setAverage(average - 1)
-  }
+  const handleClickGood = () => setContadorGood(contadorComentarioGood + 1)
+  const handleClickNeutral = () => setContadorNeutral(contadorComentarioNeutral + 1)
+  const handleClickBad = () => setContadorBad(contadorComentarioBad + 1)
 
-  const media = average / todosLosComentarios
-  const positive = ((contadorComentarioGood * 100) / todosLosComentarios)
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <BotonContador contadorGood={handleClickGood} contadorNeutral={handleClickNeutral} contadorBad={handleClickBad}/>
 
-  /* Si hay comentarios, que muestre todo, si no, mostrar SinStatics */
-  if (todosLosComentarios != 0) {
-    return (
-      <div>
-        <h1>Give feedback</h1>
-        <Boton name='good' handleClick={handleClickGood} />
-        <Boton name='neutral' handleClick={handleClickNeutral} />
-        <Boton name='bad' handleClick={handleClickBad} />
+      <h1>Statistics</h1>
+      <Statistics valorGood={contadorComentarioGood} valorNeutral={contadorComentarioNeutral} valorBad={contadorComentarioBad} />
 
-        <h1>Statistics</h1>
-        <Statistics estadistica='good' valor={contadorComentarioGood} />
-        <Statistics estadistica='neutral' valor={contadorComentarioNeutral} />
-        <Statistics estadistica='bad' valor={contadorComentarioBad} />
-        <Statistics estadistica='all' valor={todosLosComentarios} />
-        <Statistics estadistica='average' valor={media.toFixed(1)} />
-        <Statistics estadistica='positive' valor={positive.toFixed(1) + '%'} />
-      </div>
-    )
-  } else {
-    return (
-      <div>
-        <h1>Give feedback</h1>
-        <Boton name='good' handleClick={handleClickGood} />
-        <Boton name='neutral' handleClick={handleClickNeutral} />
-        <Boton name='bad' handleClick={handleClickBad} />
-
-        <h1>Statistics</h1>
-        <SinStatistics />
-      </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default App
