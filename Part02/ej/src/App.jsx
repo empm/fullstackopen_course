6b4260afd08b9f99dbcc8db11194de857d24c506
emmp <emcommunitymanager@gmail.com>
2025-10-27 18:15:57 +0100

    ej2.8

--- Part02/ej/src/App.jsx
+++ Part02/ej/src/App.jsx
@@ -1,36 +1,39 @@
 import { useState } from 'react'
 
-const MostrarPersona = ({ array }) => <p>Nombre: {array}</p>
+const MostrarPersona = ({ name, phone }) => <p>{name} - {phone}</p>
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    {
+      name: 'Arto Hellas',
+      phone: '040-1234567'
+    }
   ])
-  const [newName, setNewName] = useState('')
 
-  const handleChange = (e) => {
+  const [newName, setNewName] = useState('')
+  const [newPhone, setNewPhone] = useState('')
+
+  // form name
+  const handleChangeName = (e) => {
     setNewName(e.target.value)
   }
+        
+  const handleChangePhone = (e) => {
+    setNewPhone(e.target.value)
+  }
+      // form phone
+
   const verPersona = (e) => {
-    // No quiero que se actualice
     e.preventDefault()
-    const newPerson = { name: newName }
-    console.log("New Person.name: ", newPerson.name);
-    console.log("Persons.name: ", persons.map(e => e.name));
-    console.log("Persons.filer: ", persons.filter(n => n.name == "Enrique"));
-    console.log("Persons.includes: ", persons.map(n => n.name.includes("Enrique")));
-    /**
-     * 	1.	filter() siempre devuelve un array, aunque esté vacío.
-     * → Un array vacío en un if siempre se evalúa como true.
-     * Por eso siempre entra al if, aunque no haya coincidencias.
-     */
-    
+    const newPerson = { name: newName, phone: newPhone }
+
     if (persons.some(p => p.name === newPerson.name)) {
       alert(`${newPerson.name} ya está añadido a la agenda`)
     } else {
       setPersons(persons.concat(newPerson))
     }
-
+    setNewName('')
+    setNewPhone('')
   }
 
   return (
@@ -38,7 +41,8 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={verPersona}>
         <div>
-          name: <input value={newName} onChange={handleChange} />
+          name: <input value={newName} onChange={handleChangeName} /> <br />
+          phone: <input value={newPhone} onChange={handleChangePhone} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -46,7 +50,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       {persons.map(e =>
-        <MostrarPersona key={e.name} array={e.name} />
+        <MostrarPersona key={e.name} name={e.name} phone={e.phone} />
       )}
     </div>
   )
