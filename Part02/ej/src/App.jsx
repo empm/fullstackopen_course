47aa0fbced627825764fea9afe95b2d8e9381678
emmp <emcommunitymanager@gmail.com>
2025-11-21 13:11:51 +0100

    add: styles css and finish exercise

--- Part02/ej/src/App.jsx
+++ Part02/ej/src/App.jsx
@@ -1,25 +1,36 @@
 import { useState, useEffect } from 'react'
 import api from './services/api'
+import './styles.css'
 
-// Componente para manejar errores
-const Notification = ({ message }) => {
+/* Componente para manejar errores */
+const Notification = ({ message, type }) => {
+
   if (message === null) {
     return null
   }
 
-  return (
-    <div className='error'>
-      {message}
-    </div>
-  )
+  if (type === "succes") {
+    return (
+      <div className='succes'>
+        {message}
+      </div>
+    )
+  }
+  if (type === "error") {
+    return (
+      <div className='error'>
+        {message}
+      </div>
+    )
+  }
 }
-// Componente para visualizar lista de telefono
+/* Componente para visualizar lista de telefono */
 const MostrarPersona = ({ name, phone, handleDelete }) => <p>{name} - {phone} <button onClick={handleDelete}>delete</button></p>
 
-// Componente formulario de busqueda
+/* Componente formulario de busqueda */
 const Search = ({ newSearchValue, handleChangeSearch }) => <input value={newSearchValue} type="search" name="search" id="search" placeholder="Name..." onChange={handleChangeSearch} />
 
-// Componentes formulario añadir a persona
+/* Componentes formulario añadir a persona */
 const Form = ({ addPerson, newNameValue, newPhoneValue, handleChangeName, handleChangePhone }) => {
   return (
     <form onSubmit={addPerson}>
@@ -34,13 +45,17 @@ const Form = ({ addPerson, newNameValue, newPhoneValue, handleChangeName, handle
   )
 }
 
+/* Componente principal APP */
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('')
   const [newSearch, setNewSearch] = useState()
   // Manejar errores
-  const [errorMessage, setErrorMessage] = useState('')
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [typeMessage, setTypeMessage] = useState('')
+
+  const seconds = 5000
 
   // form name
   const handleChangeName = (e) => setNewName(e.target.value)
@@ -51,13 +66,14 @@ const App = () => {
   // form search
   const handleChangeSearch = (e) => setNewSearch(e.target.value)
 
-  // GET
+  // GET all persons
   const hookGet = () => {
     api.getAll()
       .then(data => setPersons(data))
   }
   useEffect(hookGet, [])
 
+  // Filter search
   const contactToShow = !newSearch
     ? persons
     : persons.filter(p => p.name.toLowerCase().includes(newSearch))
@@ -66,6 +82,7 @@ const App = () => {
   const addPerson = (e) => {
     e.preventDefault()
 
+    // Person constructor
     const newPerson = { name: newName, number: newPhone }
 
     const buscarPorNombre = persons.some(p => p.name === newPerson.name)
@@ -75,28 +92,42 @@ const App = () => {
     if (buscarPorNombre) {
       // Si tiene distinto telefono
       if (window.confirm(`Quieres modificar el numero de ${newName}`)) {
+        // Modificar el numero de una persona y actualizar su estado
         api.modifyPhone(obtenerPersona.id, newPerson)
           .then(updatedPerson => {
-            // Actualizar el estado con la persona modificada
-            setPersons(
-              persons.map(p =>
-                p.id === obtenerPersona.id ? updatedPerson : p
-              )
-            )
+            setPersons(persons.map(p => p.id === obtenerPersona.id ? updatedPerson : p))
+            setErrorMessage(`${newName} is modify succesfully`)
+            setTypeMessage("succes")
+            setTimeout(() => {
+              setErrorMessage(null)
+            }, seconds)
           })
-          .catch(err => {
+          /*.catch(err => {
             // Mensaje que aparece en pantalla
-            setErrorMessage(`${obtenerPersona.name} ha sido eliminado`)
+            setErrorMessage(`${obtenerPersona.name} ha sido eliminado o no se encuentra`)
+            setTypeMessage("error")
+            
             // Espera tres segundos y lo setea a null para no mostrar nada
             setTimeout(() => {
               setErrorMessage(null)
             }, 3000)
           })
+            */
       }
 
     } else {
+      // Añadir una nueva persona a la db y lista
       api.addNew(newPerson)
-        .then(p => setPersons(persons.concat(p)))
+        .then(p => {
+          setPersons(persons.concat(p))
+          setErrorMessage(`Added ${newPerson.name} to phonebook`)
+          setTypeMessage("succes")
+          setTimeout(() => { setErrorMessage(null) }, seconds)
+        })
+        /*.catch(err => {
+          setErrorMessage('ERROR')
+        })
+          */
     }
     setNewName('')
     setNewPhone('')
@@ -112,20 +143,20 @@ const App = () => {
       // Eliminar del backen
       api.deletePerson(p.id)
         .then(res => {
-        console.log("Persona eliminada: ", res)
-      // Eliminar del front (hook) - Con esto se actualiza el front sin refrescar
-        setPersons(persons.filter(per => per.id !== p.id))
+          console.log("Persona eliminada: ", res)
+          // Eliminar del front (hook) - Con esto se actualiza el front sin refrescar
+          setPersons(persons.filter(per => per.id !== p.id))
+          setErrorMessage(`${p.name} was deleted succesfully`)
+          setTypeMessage("succes")
+          setTimeout(() => { setErrorMessage(null) }, seconds)
         })
-        // TODO: 
-        // -[ ] Falta que funcione bien este metodo
-        .catch(err => {
-          // Mensaje que aparece en pantalla
-          setErrorMessage(`${obtenerPersona.name} ha sido eliminado`)
-          // Espera tres segundos y lo setea a null para no mostrar nada
-          setTimeout(() => {
-            setErrorMessage(null)
-          }, 3000)
+        /*.catch(err => {
+            setErrorMessage(`${p.name} ha sido eliminado o no se encuentra`)
+            setTypeMessage("error")
+            setTimeout(() => { setErrorMessage(null) }, seconds)
         })
+            */
+            
     }
   }
 
@@ -133,7 +164,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <Notification message={errorMessage} />
+      <Notification message={errorMessage} type={typeMessage} />
       <Search newSearchValue={newSearch} handleChangeSearch={handleChangeSearch} />
       <h2>Add a new</h2>
       <Form addPerson={addPerson} newNameValue={newName} newPhoneValue={newPhone} handleChangeName={handleChangeName} handleChangePhone={handleChangePhone} />
